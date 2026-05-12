5bd19e284e02d0c29986463a088a84ace8dacb57
Rohan Singh <ff3870422@gmail.com>
2026-05-12 21:51:12 +0530

    minor changes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "ec2_public_ips" {
-  value       = aws_instance.ec2_instance.public_ip
+  value       = [for ip in aws_instance.ec2_instance[*].public_ip : ip]
   description = "Public IP Address of the EC2 Instance"
 }
 
 output "ec2_ssh_connect" {
   description = "Command to connect to EC2 using SSH"
-  value       = "ssh -i ${var.ec2_private_key_path} ubuntu@${aws_instance.ec2_instance[count.index].public_ip}"
+  value       = [for ip in aws_instance.ec2_instance[*].public_ip : "ssh -i ${var.ec2_private_key_path} ubuntu@${ip}"]
 }
