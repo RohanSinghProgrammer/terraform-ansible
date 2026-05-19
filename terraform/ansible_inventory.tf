9bacb58a8fba63a181595f5d4614874e40a617b6
rohansingh-panini8 <rohan.singh@panini8.com>
2026-05-19 12:47:32 +0530

    tailor ansible config

--- terraform/ansible_inventory.tf
+++ terraform/ansible_inventory.tf
@@ -1,8 +1,26 @@
-resource "local_file" "ansible_inventory" {
-  filename = "../ansible/inventory.ini"
+# resource "local_file" "dev_ansible_inventory" {
+#   filename = "../ansible/dev_inventory.ini"
 
-  content = templatefile("${path.module}/inventory.tpl", {
-    ips         = module.prod_env.ec2_public_ips
+#   content = templatefile("${path.module}/templates/inventory.tpl", {
+#     ips         = module.dev_env.ec2_public_ips
+#     private_key = "../keys/ec2_key"
+#   })
+# }
+
+resource "local_file" "staging_ansible_inventory" {
+  filename = "../ansible/staging_inventory.ini"
+
+  content = templatefile("${path.module}/templates/inventory.tpl", {
+    ips         = module.staging_env.ec2_public_ips
     private_key = "../keys/ec2_key"
   })
 }
+
+# resource "local_file" "prod_ansible_inventory" {
+#   filename = "../ansible/prod_inventory.ini"
+
+#   content = templatefile("${path.module}/templates/inventory.tpl", {
+#     ips         = module.prod_env.ec2_public_ips
+#     private_key = "../keys/ec2_key"
+#   })
+# }
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,18 +3,18 @@ resource "aws_key_pair" "shared_ec2_key" {
   public_key = file("keys/ec2_key.pub")
 }
 
-module "dev_env" {
-  source = "./modules/"
-  env = "dev"
-  ec2_key_name = aws_key_pair.shared_ec2_key.key_name
-  ami_id = "ami-07a00cf47dbbc844c"
-  ec2_region = "ap-south-1"
-  instance_type = "t3.micro"
-  instance_count = 1
-  ebs_size = 16
-  ebs_type = "gp3"
-  ec2_private_key_path = "../keys/ec2_key"
-}
+# module "dev_env" {
+#   source = "./modules/"
+#   env = "dev"
+#   ec2_key_name = aws_key_pair.shared_ec2_key.key_name
+#   ami_id = "ami-07a00cf47dbbc844c"
+#   ec2_region = "ap-south-1"
+#   instance_type = "t3.micro"
+#   instance_count = 1
+#   ebs_size = 16
+#   ebs_type = "gp3"
+#   ec2_private_key_path = "../keys/ec2_key"
+# }
 
 module "staging_env" {
   source = "./modules/"
@@ -29,15 +29,15 @@ module "staging_env" {
   ec2_private_key_path = "../keys/ec2_key"
 }
 
-module "prod_env" {
-  source = "./modules/"
-  env = "prod"
-  ec2_key_name = aws_key_pair.shared_ec2_key.key_name
-  ami_id = "ami-07a00cf47dbbc844c"
-  ec2_region = "ap-south-1"
-  instance_type = "t3.medium"
-  instance_count = 2
-  ebs_size = 24
-  ebs_type = "gp3"
-  ec2_private_key_path = "../keys/ec2_key"
-}
+# module "prod_env" {
+#   source = "./modules/"
+#   env = "prod"
+#   ec2_key_name = aws_key_pair.shared_ec2_key.key_name
+#   ami_id = "ami-07a00cf47dbbc844c"
+#   ec2_region = "ap-south-1"
+#   instance_type = "t3.medium"
+#   instance_count = 2
+#   ebs_size = 24
+#   ebs_type = "gp3"
+#   ec2_private_key_path = "../keys/ec2_key"
+# }
